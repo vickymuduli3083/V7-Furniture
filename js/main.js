0c71142ac0ebe9be51202218f3944ad785203ef4
vicky muduli <vickymuduli3083@gmail.com>
2024-06-18 15:27:42 +0530

    some changes

--- js/main.js
+++ js/main.js
@@ -537,3 +537,26 @@
     });
 
 })(jQuery);
+
+$('#myTab a').on('click', function(e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
+
+/**
+ * Datatable call
+ */
+$(document).ready(function() {
+    $('#my-orders-table').DataTable();
+});
+
+/**
+ * My account nav click
+ */
+$(document).ready(function() {
+    $('.tg-tabs-content-wrapp .my-account-dashboard .card').click(function() {
+
+        var ariaClick = $(this).attr('area-toggle');
+        $('.tg-account .account-banner .nav-area  a#' + ariaClick).click();
+    });
+});
